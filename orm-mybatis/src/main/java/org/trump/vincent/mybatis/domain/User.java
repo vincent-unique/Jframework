bd697fe1ad906310ac0751a5e60b3b4101d59f9f
vincent <vincent_unique@163.com>
2018-01-21 08:40:22 +0800

    update orm-mybatis

---
+++ orm-mybatis/src/main/java/org/trump/vincent/mybatis/domain/User.java
@@ -0,0 +1,61 @@
+package org.trump.vincent.mybatis.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    private static final long serialVersionUID = 100432L;
+    public String getUserId() {
+        return userId;
+    }
+
+    public User setUserId(String userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public User setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public User setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public SexEnum getSex() {
+        return sex;
+    }
+
+    public User setSex(SexEnum sex) {
+        this.sex = sex;
+        return this;
+    }
+
+    private String userId;
+    private String userName;
+    private String password;
+    private int age;
+    private SexEnum sex;
+}
+enum  SexEnum implements Serializable{
+    MALE,
+    FEMALE
+}
--- orm-mybatis/src/main/webapp/WEB-INF/content/welcome.jsp
+++ orm-mybatis/src/main/webapp/WEB-INF/content/welcome.jsp
@@ -6,11 +6,38 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="mvc" uri="http://www.springframework.org/tags" %>
 <html>
 <head>
     <title>Hello</title>
 </head>
 <body>
 <h2>${requestScope.message}</h2>
+
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<form:form acceptCharset="UTF-8" action="register" method="post">
+    <table>
+        <tr>
+            <td>姓名：</td>
+            <td>
+                <form:input path="username"/>
+            </td>
+        </tr>
+
+        <tr>
+            <td>性别：</td>
+            <td>
+                <form:input path="sex"/>
+            </td>
+        </tr>
+
+        <tr>
+            <td>年龄：</td>
+            <td>
+                <form:input path="age"/>
+            </td>
+        </tr>
+    </table>
+</form:form>
 </body>
 </html>

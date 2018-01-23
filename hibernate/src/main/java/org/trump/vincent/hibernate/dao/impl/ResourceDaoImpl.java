d79e0e1df937d0f0ecc7a6466498512728fd723c
vincent <vincent_unique@163.com>
2018-01-23 21:50:50 +0800

    update for the module hibernate

--- hibernate/src/main/java/org/trump/vincent/hibernate/dao/impl/ResourceDaoImpl.java
+++ hibernate/src/main/java/org/trump/vincent/hibernate/dao/impl/ResourceDaoImpl.java
@@ -1,6 +1,8 @@
 package org.trump.vincent.hibernate.dao.impl;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Expression;
 import org.hibernate.query.Query;
 import org.trump.vincent.hibernate.core.SingletonSessionFactory;
 import org.trump.vincent.hibernate.dao.ResourceDao;
@@ -15,24 +17,41 @@ public class ResourceDaoImpl implements ResourceDao {
 
     public Resource findById(String id) {
         Session session = SingletonSessionFactory.getSessionFactory().openSession();
-        session.beginTransaction();
+//        session.beginTransaction();
         String hql = "FROM org.trump.vincent.hibernate.domain.Resource WHERE ID =:resourceId";
         Query<Resource> resourceQuery = session.createQuery(hql,Resource.class);
         resourceQuery.setString("resourceId",id);
         Resource resource = resourceQuery.getSingleResult();
 
-        session.getTransaction().commit();
+//        session.getTransaction().commit();
+//        session.close();
         return resource;
     }
 
     public List<Resource> loadByName(String name) {
         Session session = SingletonSessionFactory.getSessionFactory().openSession();
-        session.beginTransaction();
+//        session.beginTransaction();
 
         String hql = "FROM org.trump.vincent.hibernate.domain.Resource WHERE ID LIKE :resourceName";
         Query<Resource> resourceQuery = session.createQuery(hql);
         resourceQuery.setString("resourceName",name);
         List<Resource> resourceList = resourceQuery.list();
+//        session.getTransaction().commit();
+//        session.close();
+        return resourceList;
+    }
+
+    @Deprecated
+    public List<Resource> loadByName2(String name) {
+        Session session = SingletonSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        Criteria userCriteria = session.createCriteria(Resource.class);
+        userCriteria.add(Expression.eq("name",name));
+        List<Resource> resourceList = userCriteria.list();
+
+        session.getTransaction().commit();
+        session.close();
         return resourceList;
     }
 }

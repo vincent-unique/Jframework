9bfc21ef24ba037c37a3e3275d9728064e284304
vincent <vincent_unique@163.com>
2018-01-23 20:48:56 +0800

    update mybatis configurations

---
+++ hibernate/src/main/java/org/trump/vincent/hibernate/domain/Resource.java
@@ -0,0 +1,7 @@
+package org.trump.vincent.hibernate.domain;
+
+/**
+ * Created by Vincent on 2018/1/23 0023.
+ */
+public class Resource {
+}
---
+++ hibernate/src/main/java/org/trump/vincent/hibernate/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Vincent on 2018/1/23 0023.
+ */
+package org.trump.vincent.hibernate;
